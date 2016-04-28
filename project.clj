cd693e24069cbb3091a8b32d35beffe55c2b5b61
Minh Tuan Nguyen <minhtuannguyen2704@gmail.com>
2016-04-28 14:27:12 +0200

    pump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-git "0.1.0"
+(defproject lein-git "0.2.0-SNAPSHOT"
   :description "lein-git is a leiningen-plugin which has been made in order to make commit whose messages follow certain pattern"
   :url "https://github.com/minhtuannguyen/lein-git"
   :license {:name "Eclipse Public License"
